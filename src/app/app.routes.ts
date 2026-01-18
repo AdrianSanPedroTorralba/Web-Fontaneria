41afc9316b27406418a79ff01fd5ea36547401a5
JavierPrior845 <javierprior04@gmail.com>
2026-01-18 14:24:36 +0100

    Componente contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
+import { Contacto } from './pages/contacto/contacto';
 
 export const routes: Routes = [
-  { path: '', component: Inicio }
+  { path: '', component: Inicio },
+  {
+    path: 'contacto',
+    component: Contacto,
+    title: 'Contacto'
+  }
 ];
 
