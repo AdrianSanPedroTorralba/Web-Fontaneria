029fc8a6f8aba2d6f6baf3f35e9e3cbe5be03f5e
AdrianSanPedroTorralba <adriansapedro758@gmail.com>
2026-01-19 19:34:00 +0100

    componente sobre nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Contacto } from './pages/contacto/contacto';
+import { SobreNosotros } from './pages/sobre-nosotros/sobre-nosotros';
 
 export const routes: Routes = [
   { path: '', component: Inicio },
@@ -8,6 +9,11 @@ export const routes: Routes = [
     path: 'contacto',
     component: Contacto,
     title: 'Instalaciones PTV-Contacto'
+  },
+  {
+    path: 'sobreNosotros',
+    component: SobreNosotros,
+    title: 'Instalaciones PTV-Sobre-Nosotros'
   }
 ];
 
