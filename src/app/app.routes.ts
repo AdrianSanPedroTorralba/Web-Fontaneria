720e3d68522fa5bb14e67e467a6c2586b4d3723d
JavierPrior845 <javierprior04@gmail.com>
2026-01-19 19:31:34 +0100

    Feat: servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Contacto } from './pages/contacto/contacto';
+import { Servicio } from './pages/servicio/servicio';
 
 export const routes: Routes = [
   { path: '', component: Inicio },
@@ -8,6 +9,11 @@ export const routes: Routes = [
     path: 'contacto',
     component: Contacto,
     title: 'Instalaciones PTV-Contacto'
+  },
+  {
+    path: 'servicios',
+    component: Servicio,
+    title: 'Instalaciones PTV-Servicios'
   }
 ];
 
